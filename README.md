b4fbd44ea792faf1521ae47428aa5698cf5d8ed9
DOSSA-HASAN <hassandossa5@gmail.com>
2024-08-16 15:24:36 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# To-Do List Application
+
+## Description
+
+This is a simple To-Do List application built with React. It allows users to add tasks, delete tasks, and rearrange the order of tasks within the list.
+
+## Features
+
+- **Add a Task**: Users can input a task and add it to the list.
+- **Delete a Task**: Users can remove any task from the list.
+- **Move Task Up**: Users can move a task one position up in the list.
+- **Move Task Down**: Users can move a task one position down in the list.
+
---
+++ src/ToDoList.jsx
@@ -0,0 +1,67 @@
+import React,{useState} from "react"
+
+function ToDoList(){
+
+    const [tasks, setTask] = useState([])
+    const [newTask, setNewTask] = useState("")
+
+    function handleInputChange(event){
+        setTask(event.target.value)
+    }
+
+    function handleAddTask(event){
+        
+        if(newTask.trim() !== ""){
+            setTask(t => [...t, newTask])
+            setNewTask("")
+        }
+        
+    }
+
+    function handleDeleteTask(index){
+        const updateTask = tasks.filter((_, i) => i !== index);
+        setTask(updateTask)
+
+    }
+
+    function moveTaskUp(index){
+        if(index > 0){
+            const updateTask = [...tasks];
+            [updateTask[index], updateTask[index - 1]] = [updateTask[index -1], updateTask[index]];
+            setTask(updateTask);
+        }
+    }
+
+    function moveTaskDown(index){
+        if(index < tasks.length - 1){
+            const updateTask = [...tasks];
+            [updateTask[index], updateTask[index + 1]] = [updateTask[index + 1], updateTask[index]]
+            setTask(updateTask)
+        }
+    }
+
+
+    return(
+        <>
+            <div className="to-do-list">
+                <h1>To Do List</h1>
+
+                <div>
+                    <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange}/>
+                    <button className="add-button" onClick={(index) => handleAddTask(index)}>Add task</button>
+                </div>
+
+                <ol>
+                    {tasks.map((task, index) => <li key={index}><span className="text">{task}</span>
+                    <button className="delete-button" onClick={() => handleDeleteTask(index)}>Delete</button>
+                    <button className="move-up-button" onClick={() => moveTaskUp(index)}>👆</button>
+                    <button className="move-down-button" onClick={() => moveTaskDown(index)}>👇</button>
+                    </li>)}
+                </ol>
+            </div>
+        </>
+    )
+
+}
+
+export default ToDoList
